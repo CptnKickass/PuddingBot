483eba4a9e57f0bb471c9181915d25b273e509c4
goose <goose@captain-kickass.net>
2014-02-04 05:38:20 -0500

    First commit

---
+++ controller.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+########################################################################
+#                                                                      #
+#             DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE              #
+#                     Version 2, December 2004                         #
+#                                                                      #
+#  Copyright (c) 2014 goose <goose@captain-kickass.net>                #
+#                                                                      #
+#  Everyone is permitted to copy and distribute verbatim or modified   #
+#  copies of this license document, and changing it is allowed as long #
+#  as the name is changed.                                             #
+#                                                                      #
+#             DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE              #
+#    TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION   #
+#                                                                      #
+#   0. You just DO WHAT THE FUCK YOU WANT TO.                          #
+#                                                                      #
+########################################################################
+
+## Version 1.0.0
+
+## Changelog
+# 1.0.0 - Initial commit
+
+## Todo
+# None, yet
+
+## Config
+
+## Source
