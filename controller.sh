314bb1f892e911a3740d36586e624d76485c1ee6
goose <goose@captain-kickass.net>
2014-02-04 23:11:54 -0500

    Updated README, and added some ground work for the controller. Also created the modules directory, though that won't be public for some time.

--- controller.sh
+++ controller.sh
@@ -40,3 +40,86 @@
 ## Config
 
 ## Source
+
+# Define some variables.
+ver="$(fgrep -m 1 "## Version" "${0}" | awk '{print $3}')"
+
+# Check for args for non interactive mode. If not present, start in interactive mode.
+if [ -n "${1}" ]; then
+	# Convert ${1} to lowercase
+	arg="${1,,}"
+	case "${arg}" in
+		--start)
+		;;
+		--stop)
+		;;
+		--restart)
+		;;
+		--status)
+		;;
+		--force-start)
+		;;
+		--force-stop)
+		;;
+		--version)
+			echo "Version: ${ver}"
+		;;
+		--help)
+			echo "Available commands: --start --stop --restart --status --force-start --force-stop --version"
+		;;
+		*)
+			echo "Invalid option! Available commands:"
+			echo "--start --stop --restart --status --force-start --force-stop --version"
+			exit 1
+		;;
+	esac
+exit 0
+fi
+
+# If we bypassed the above if statement, we must be starting in interactive mode.
+clear
+while true; do
+	echo "PuddingBot Interactive Console."
+	echo "Please choose an option:"
+	echo ""
+	echo "[1] Start bot"
+	echo "[2] Stop bot"
+	echo "[3] Restart bot"
+	echo ""
+	echo "[4] Check bot status"
+	echo ""
+	echo "[5] Force start bot"
+	echo "[6] Force stop bot"
+	echo ""
+	echo "[7] Print bot version"
+	echo ""
+	echo "[8] Exit console"
+	echo ""
+	echo "Please choose a number:"
+	read -p "> " opt
+	clear
+	case "$opt" in
+		1)
+		;;
+		2)
+		;;
+		3)
+		;;
+		4)
+		;;
+		5)
+		;;
+		6)
+		;;
+		7)
+		;;
+		8)
+			echo "Goodbye."
+			exit 0
+		;;
+		*)
+			echo "Invalid option!"
+		;;
+	esac
+	echo ""
+done
