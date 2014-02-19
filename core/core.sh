20894b1c6ba5d96cac0a1e019b8089e875386005
goose <goose@captain-kickass.net>
2014-02-19 15:37:47 -0500

    We're now able to connect to servers, process user and server messages, and the output is now dumped to a centralized function which will make sure we don't dump too much text at once

--- core/core.sh
+++ core/core.sh
@@ -12,6 +12,10 @@ done
 # Load admins into the core
 if [ -e "admins.conf" ]; then
 	echo "Loading admins into bot"
+	if [ -e "var/.admins" ]; then
+		rm -f var/.admins
+	fi
+	touch var/.admins
 else
 	echo "Unable to locate bot admins config!"
 	exit 1
@@ -55,7 +59,7 @@ startTime="$(date +%s)"
 inArray() {
 # When passing items to this to see if they're in the array or not,
 # the format should be:
-# inArray "${itemToBeCheck}" "${arrayToCheck}"
+# inArray "${itemToBeCheck}" "${arrayToCheck[@]}"
 # If it is in the array, it'll return the boolean of inArr=1.
 local n=$1 h
 shift
@@ -69,8 +73,19 @@ done
 }
 
 parseOutput () {
-unset outArr
-while IFS= read -rn256 -d '' outArr[i++]; do :; done <<< "$out"
+echo "[DEBUG] Parse Output hit"
+if [ "${#outArr[@]}" -ne "0" ]; then
+	echo "[DEBUG] If is true"
+	unset sendArr
+	while IFS= read -rn256 -d '' sendArr[i++]; do :; done <<< "${outArr[@]}"
+	unset outArr
+	echo "[DEBUG] \${sendArr[@]}: ${sendArr[@]}"
+	echo "[DEBUG] \${#sendArr[@]}: ${#sendArr[@]}"
+	for line in "${sendArr[@]}"; do
+		echo "PRIVMSG ${senderTarget} :${line}" >> $output
+		sleep 0.25
+	done
+fi
 }
 
 echo "Creating datafile"
@@ -101,7 +116,7 @@ do
 		echo "Place holder"
 	fi
 	# The incoming messages should be in one of the following formats:
-	# :${botNick} (Bot setting modes on itself)
+	# :${nick} (Bot setting modes on itself)
 	# :n!u@h (Another client)
 	# :${server} (The IRCd server)
 	# PING (PING's from the IRCd server)
@@ -110,10 +125,10 @@ do
 		echo "PONG${message#PING}" >> $output
 	elif [ "$(echo "$message" | awk '{print $1}')" == ":${nick}" ]; then
 		# The bot is changing modes on itself
-		./core/botmodechange.sh "$message"
+		#./core/botmodechange.sh "$message"
+		echo "Place holder"
 	elif [ "$(echo "$message" | awk '{print $1}' | egrep -c "^:.*!.*@.*$")" -eq "1" ]; then
 		# The message matches an n!u@h mask
-		#./core/usermessage.sh "$message"
 		senderTarget="$(echo "$message" | awk '{print $3}')"
 		senderAction="$(echo "$message" | awk '{print $2}')"
 		senderFull="$(echo "$message" | awk '{print $1}')"
@@ -122,122 +137,25 @@ do
 		senderUser="${senderFull#*!}"
 		senderUser="${senderUser%@*}"
 		senderHost="${senderFull#*@}"
-		
-		if [ "$senderTarget" = "$nick" ]; then
-			# It's a PM. 
-			senderTarget="$senderNick"
+		out="$(./core/usermessage.sh "$message")"
+		if [ -n "$out" ]; then
+			outArr=("$out")
 		fi
-		
-		case "$(echo "$message" | awk '{print $2}')" in
-			JOIN) 
-				;;
-			KICK)
-				;;
-			NOTICE)
-				;;
-			PRIVMSG)
-				if [ "$(echo "$message" | awk '{print $4}' | cut -b 2)" == "${comPrefix}" ]; then
-					com="$(echo "$message" | awk '{print $4}' | tr "[:upper:]" "[:lower:]")"
-					com="${com:2}"
-					case "$com" in
-						login)
-							inArray "${senderUser}@${senderHost}" "${admins[@]}"
-							if [ "$inArr" -eq "0" ]; then
-								lUser="$(echo "$message" | awk '{print $5}')"
-								lPass="$(echo "$message" | awk '{print $6}')"
-								lPass="$(echo "$lPass" | md5sum | awk '{print $1}')"
-								lPass2="$(echo "$lPass" | md5sum | awk '{print $1}')"
-								lHash="${lPass}${lPass2}"
-								if egrep -v "(^#|^\$)" admins.conf | fgrep -q "user=\"${lUser}\""; then
-									# User exists
-									if egrep -v "(^#|^\$)" admins.conf | fgrep -A 1 "user=\"${lUser}\"" | tail -n 1 | fgrep -q "pass=\"${lHash}\""; then
-										# Password matches user
-										admins=("${senderUser}@${senderHost}")
-										echo "PRIVMSG $senderTarget :Successfully logged in" >> $output
-										echo "PRIVMSG $senderTarget :\${admins[@]}: ${admins[@]}" >> $output
-									else
-										# Password does not match user
-										echo "PRIVMSG $senderTarget :Invalid login" >> $output
-									fi
-								else
-									# No such user
-									echo "PRIVMSG $senderTarget :Invalid login" >> $output
-								fi
-							else
-								echo "PRIVMSG $senderTarget :Already logged in" >> $output
-							fi
-						;;
-						logout)
-							inArray "${senderUser}@${senderHost}" "${admins[@]}"
-							if [ "$inArr" -eq "0" ]; then
-								echo "PRIVMSG $senderTarget :You are not logged in" >> $output
-							else
-								admins=("${admins[@]/${senderUser}@${senderHost}/}")
-								echo "PRIVMSG $senderTarget :\${admins[@]}: ${admins[@]}" >> $output
-							fi
-						;;
-						join)
-						if [ -z "$(echo "$message" | awk '{print $5}')" ]; then
-							echo "PRIVMSG $senderTarget :This command requires a parameter" >> $output
-						elif [ "$(echo "$message" | awk '{print $5}' | egrep -c "^(#|&)")" -eq "0" ]; then
-							echo "$(echo "$message" | awk '{print $5}') does not appear to be a valid channel" >> $output
-						else
-							echo "JOIN $(echo "$message" | awk '{print $5}')" >> $output
-							echo "PRIVMSG $senderTarget :Joined $(echo "$message" | awk '{print $5}')" >> $output
-						fi
-						;;
-						part)
-						if [ -z "$(echo "$message" | awk '{print $5}')" ]; then
-							echo "PRIVMSG $senderTarget :This command requires a parameter" >> $output
-						elif [ "$(echo "$message" | awk '{print $5}' | egrep -c "^(#|&)")" -eq "0" ]; then
-							echo "$(echo "$message" | awk '{print $5}') does not appear to be a valid channel" >> $output
-						else
-							echo "PART $(echo "$message" | awk '{print $5}')" >> $output
-							echo "PRIVMSG $senderTarget :Left $(echo "$message" | awk '{print $5}')" >> $output
-						fi
-						;;
-						status)
-							echo "PRIVMSG $senderTarget :I am $nick, currently connected to $server (${actualServer:1} on ${networkName}) via port $port. I am hosted on $(uname -n). My PID is $$. My owner is $owner ($ownerEmail)." >> $output
-						;;
-						uptime)
-							timeDiff="$(( $(date +%s) - $startTime ))"
-							days=$((timeDiff/60/60/24))
-							hours=$((timeDiff/60/60%24))
-							minutes=$((timeDiff/60%60))
-							seconds=$((timeDiff%60))
-							echo "PRIVMSG $senderTarget :Uptime: $days days, ${hours} hours, ${minutes} minutes, ${seconds} seconds" >> $output
-						;;
-					esac
-				fi
-				;;
-			QUIT)
-				;;
-			MODE)
-				;;
-			PART) 
-				;;
-			NICK)
-				;;
-			WALLOPS)
-				;;
-			TOPIC)
-				;;
-			INVITE)
-				;;
-			*)
-				echo "$(date) | Received unknown message level 3: ${message}" >> ${dataDir}/$$.debug
-				;;
-		esac
 	elif [ "$(echo "$message" | awk '{print $1}' | egrep -c "^:.*!.*@.*$")" -eq "0" ]; then
 		# The message does not match an n!u@h mask, and should be a server
 		out="$(./core/servermessage.sh "$message")"
 		if [ -n "$out" ]; then
-			echo "$out" >> $output
+			outArr=("$out")
 		fi
 	else
 		# This should never be reached, but exists for debug purposes
+		outArr=("[DEBUG-core.sh] $message")
 		echo "$(date -R): $message" >> $$.debug
 	fi
+	echo "[DEBUG] \$out: $out"
+	echo "[DEBUG] \${outArr}[@]: ${outArr[@]}"
+	echo "[DEBUG] \${#outArr[@]}: ${#outArr[@]}"
+	parseOutput;
 done
 
 # We shouldn't ever break out of the above loop. If we do, something went wrong.
