21a997ee5177a7e26d840fcfc9b62099b412ddfd
goose <goose@captain-kickass.net>
2015-01-25 12:34:21 -0500

    This is no longer needed

--- contrib/nikiwatch.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-if [ -e /home/goose/PuddingBot/var/outbound ]; then
-	echo "PRIVMSG #goose :[WATCHER] ${1} has been updated with new data" >> /home/goose/PuddingBot/var/outbound
-fi
-exit 0
