cdb118a2a6807121e80d371a147b81d01d919a32
goose <goose@captain-kickass.net>
2015-04-19 12:16:36 -0400

    .

--- bin/user/commands/logout.sh
+++ bin/user/commands/logout.sh
@@ -4,13 +4,17 @@ loggedIn="$(fgrep -c "${senderUser}@${senderHost}" "var/.admins")"
 if [[ "${loggedIn}" -eq "0" ]]; then
 	echo "You are not logged in"
 else
-	if [[ "$(fgrep "${senderUser}@${senderHost}" "var/.admins" | awk '{print $2}')" -eq "1" ]]; then
+	aLine="$(fgrep -n "${senderUser}@${senderHost}" "var/.admins")"
+	lineNo="${aLine%%:*}"
+	aLine="${aLine#${lineNo}:}"
+	sed -i "${lineNo}d" "var/.admins"
+	if [[ "$(awk '{print $2}' <<<"${aLine}")" -eq "1" ]]; then
 		# Only 1 clone logged in, so we can delete their entry
-		sed -i "/${senderUser}@${senderHost}/d" "var/.admins"
 		echo "Successfully logged out"
 	else
 		# More than 1 clone logged in. Remove only the appropriate u@h
-		sed -i "s/ ${senderUser}@${senderHost}//" "var/.admins"
+		newLine="${aLine/${senderUser}@${senderHost}/}"
+		echo "${newLine}" >> "var/.admins"
 		echo "Successfully logged out"
 	fi
 fi
